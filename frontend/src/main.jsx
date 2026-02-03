e862e6cd8e34a389147c334fd60b56832b2f396e
Kanishk <kanishkpandey1701@gmail.com>
2026-02-03 21:14:19 +0530

    Redux Ready

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' 
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux' // <--- IMPORT THIS
+import store from './store.js'         // <--- IMPORT THIS
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> 
-      <App />
-    </BrowserRouter>
+    <Provider store={store}> {/* <--- WRAP APP IN PROVIDER */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
